55a90a819ec198a0309d1c0d5c4aa5f06cdabbe2
hhz <1165071241@qq.com>
2019-02-13 18:12:58 +0800

    PrototypePattern

---
+++ PrototypePattern/IColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    public interface IColor
+    {
+        IColor Clone();
+
+        int Red { get; set; }
+        int Green { get; set; }
+        int Blue { get; set; }
+    }
+}
---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IColor red = new ReadColor();
+                red.Red = 1;
+                IColor green = red.Clone();
+                green.Red = 2;
+
+                Console.WriteLine($"red.Red={red.Red},green.Red={green.Red}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
